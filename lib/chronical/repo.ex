348be3f8d9a338ab1bfc7fd76c944ba53477fa70
Ruan Brandão <ruan.bernardo@gmail.com>
2016-11-07 01:19:56 -0200

    Create fake Repo for users

--- lib/chronical/repo.ex
+++ lib/chronical/repo.ex
@@ -1,3 +1,19 @@
 defmodule Chronical.Repo do
-  use Ecto.Repo, otp_app: :chronical
+  # use Ecto.Repo, otp_app: :chronical
+
+  def all(Chronical.User) do
+    [%Chronical.User{id: "1", name: "Quéven", username: "mito", password: "mito"},
+     %Chronical.User{id: "1", name: "Ruan", username: "patrao", password: "patrao"}]
+  end
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
 end
