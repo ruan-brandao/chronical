d5542e847a4dee6ba704c6f2f0e436fac0b906e5
Ruan Brandão <ruan.bernardo@gmail.com>
2016-11-08 20:32:12 -0200

    Create Session Controller logout

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -17,4 +17,10 @@ defmodule Chronical.SessionController do
         |> render("new.html")
     end
   end
+
+  def delete(conn, _) do
+    conn
+    |> Chronical.Auth.logout()
+    |> redirect(to: page_path(conn, :index))
+  end
 end
