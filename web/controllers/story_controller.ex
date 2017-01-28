6b1126c9e5abbb7b2fed2caf56cbb02d72cda57c
Ruan Brandão <ruan.bernardo@gmail.com>
2017-01-28 16:42:27 -0200

    Implement management for relation between User and Story

--- web/controllers/story_controller.ex
+++ web/controllers/story_controller.ex
@@ -3,42 +3,47 @@ defmodule Chronical.StoryController do
 
   alias Chronical.Story
 
-  def index(conn, _params) do
+  def index(conn, _params, _user) do
     stories = Repo.all(Story)
     render(conn, "index.html", stories: stories)
   end
 
-  def new(conn, _params) do
-    changeset = Story.changeset(%Story{})
+  def new(conn, _params, user) do
+    changeset =
+      user
+      |> build_assoc(:stories)
+      |> Story.changeset()
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"story" => story_params}) do
-    changeset = Story.changeset(%Story{}, story_params)
-
+  def create(conn, %{"story" => story_params}, user) do
+    changeset =
+      user
+      |> build_assoc(:stories)
+      |> Story.changeset(story_params)
     case Repo.insert(changeset) do
-      {:ok, _story} ->
+      {:ok, story} ->
         conn
-        |> put_flash(:info, "Story created successfully.")
+        |> put_flash(:info, "Story created successfully")
         |> redirect(to: story_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
   end
 
-  def show(conn, %{"id" => id}) do
+  def show(conn, %{"id" => id}, _user) do
     story = Repo.get!(Story, id)
     render(conn, "show.html", story: story)
   end
 
-  def edit(conn, %{"id" => id}) do
-    story = Repo.get!(Story, id)
+  def edit(conn, %{"id" => id}, user) do
+    story = Repo.get!(user_stories(user), id)
     changeset = Story.changeset(story)
     render(conn, "edit.html", story: story, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "story" => story_params}) do
-    story = Repo.get!(Story, id)
+  def update(conn, %{"id" => id, "story" => story_params}, user) do
+    story = Repo.get!(user_stories(user), id)
     changeset = Story.changeset(story, story_params)
 
     case Repo.update(changeset) do
@@ -51,15 +56,21 @@ defmodule Chronical.StoryController do
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    story = Repo.get!(Story, id)
-
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
+  def delete(conn, %{"id" => id}, user) do
+    story = Repo.get!(user_stories(user), id)
     Repo.delete!(story)
 
     conn
     |> put_flash(:info, "Story deleted successfully.")
     |> redirect(to: story_path(conn, :index))
+  end
+
+  def action(conn, _) do
+    apply(__MODULE__, action_name(conn),
+          [conn, conn.params, conn.assigns.current_user])
+  end
+
+  defp user_stories(user) do
+    assoc(user, :stories)
   end
 end
